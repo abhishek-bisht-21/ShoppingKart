ab9519741dee6424c1191f74e7755e66479e1b46
Abhishek <abhishek.bisht54@yahoo.com>
2022-03-02 12:27:58 +0530

    Making Changes in Update and Save btn

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -15,6 +15,7 @@ export class DialogComponent implements OnInit {
 
   freshnessList = ["Brand New", "Second Hand", "Refurbished"];
   productForm !: FormGroup;
+  actionBtn:string = "Save"
   constructor(private formBuilder: FormBuilder, private api:ApiService, 
     @Inject(MAT_DIALOG_DATA) public editData:any
     ,private dialogRef:MatDialogRef<DialogComponent>) { }
@@ -32,6 +33,7 @@ export class DialogComponent implements OnInit {
 
     // Patching all the values
     if(this.editData){
+      this.actionBtn = "Update"
       this.productForm.controls['productName'].setValue(this.editData.productName);
       this.productForm.controls['category'].setValue(this.editData.category);
       this.productForm.controls['date'].setValue(this.editData.date);
